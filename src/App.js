4c97043d6eb0e4c0c48ea8d2a98dc7b644482875
nika <nikamunja31415@gmail.com>
2023-10-04 20:40:07 +0400

    add popup component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
-import { useState } from "react";
-import SignIn from "./components/layouts/SignIn";
-import Todo from "./components/layouts/Todo";
-import AccordionApp from "./components/_atoms/AccordionApp";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [show, setShow] = useState(false);
+  const [showModal,setShowModal] = useState(false);
 
-  // const handleChangeValue = (email,password) => {
-  //   console.log({email,password})
-  //   setShow(true);
-  // }
+  useEffect(() => {
+    setShowModal(true);
+  }, []);
 
   return (
     <div className="App">
-      {/* {!show && <SignIn handleChangeValue={handleChangeValue} />}
-
-      {show && (
-        <div>
-          <h1>Hello This is HomePage!!</h1>
-        </div>
-      )} */}
-      {/* <Todo />
-      <button onClick={() => setShow(!show)}>Show</button>
-
-      {show ? (
-       <div className="darkMode">
-        <h1>Hello</h1>
-       </div>
-      ) : (
-        <div className="lightMode">
-        <h1>Hello</h1>
-       </div>
-      )} */}
-
-      <AccordionApp />
+    {showModal && <Modal setShowModal={setShowModal} showModal={showModal}/>}
     </div>
   );
 }
 
 export default App;
+
+const Modal = ({showModal,setShowModal}) => {
+  return (
+    <div style={{width: 200, height: 200, backgroundColor: 'red'}}>
+      <button onClick={() => setShowModal(!showModal)}>X</button>
+      <h1>Hello From Modal!!</h1>
+    </div>
+  )
+}
